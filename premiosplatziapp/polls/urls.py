6102f17f1dc12e98f09c79fd65941302c99a4322
bremys <bremysromero@gmail.com>
2022-02-21 17:35:58 -0500

    agregando urls y la vistas

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
 from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<int:question_id>/", views.detalle, name="index"),
+    path("<int:question_id>/resultados/", views.resultados, name="index"),
+    path("<int:question_id>/voto/", views.voto, name="index"),
 ]
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Dios te bendiga")
+    return HttpResponse("Estas en la pagina principal de premios Platzi")
+
+
+def detalle(request, question_id):
+    return HttpResponse(F"Estas viendo la pregunta numero {question_id}")
+
+
+def resultados(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
+
+
+def voto(request, question_id):
+    return HttpResponse(f"Estas votado a la pregunta numero {question_id}")
