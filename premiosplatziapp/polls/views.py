3cd40a23c32f158503ea1fc5e8fb011180372cd2
bremys <bremysromero@gmail.com>
2022-02-21 20:47:14 -0500

    modificando el archivo views y templates

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Questions
+
 
 def index(request):
-    return HttpResponse("Estas en la pagina principal de premios Platzi")
-
+    latest_question_list = Questions.objects.all()
+    return render(request, "polls/index.html", {
+        'latest_question_list': latest_question_list
+    } )
 
 def detalle(request, question_id):
-    return HttpResponse(F"Estas viendo la pregunta numero {question_id}")
+    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
 
 
 def resultados(request, question_id):
