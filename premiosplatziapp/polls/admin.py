5191d0923e9408db9a3d1082cf2177ae09fbb763
bremys <bremysromero@gmail.com>
2022-02-24 12:14:52 -0500

    añadiendo estilos css

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -1,7 +1,20 @@
+from dataclasses import fields
+from re import search
 from django.contrib import admin
 from .models import Questions, Choices
 
+class ChoiceInLine(admin.StackedInline):
+    model = Choices
+    extra = 3
 
-admin.site.register(Questions)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+    inlines = [ChoiceInLine]
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+admin.site.register(Questions, QuestionAdmin)
 admin.site.register(Choices) 
 
